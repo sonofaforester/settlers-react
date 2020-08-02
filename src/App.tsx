97bd0158ac9bb74c95e8d1b3314e5ae469a4866e
Mark Edwards <medwards@kcftech.com>
2020-08-02 17:33:56 -0400

    Smarter town building

--- src/App.tsx
+++ src/App.tsx
@@ -19,173 +19,184 @@ import { playerHasWon } from './utils/scoring';
 import { getCurrentPlayerColor, rollADie } from './utils/utils';
 
 interface IAppProps {
-  dispatchBotMove: (botAction: IBotMakeTurnAction) => any,
-  dispatchBuildRoad: (targetEdge: IEdge) => any,
-  dispatchBuildTown: (targetVtx: number) => any,
-  dispatchDistributeResources: (dieRoll: number) => any;
-  dispatchEndPlayerTurn: () => any,
-  dispatchInitialMove1: (townVertex: number, roadEdge: IEdge) => any;
-  dispatchInitialMove2: (townVertex: number, roadEdge: IEdge) => any;
-  dispatchPlayerNames: (playerNames: any) => any;
-  gameState: ICatanState;
+    dispatchBotMove: (botAction: IBotMakeTurnAction) => any
+    dispatchBuildRoad: (targetEdge: IEdge) => any
+    dispatchBuildTown: (targetVtx: number) => any
+    dispatchDistributeResources: (dieRoll: number) => any
+    dispatchEndPlayerTurn: () => any
+    dispatchInitialMove1: (townVertex: number, roadEdge: IEdge) => any
+    dispatchInitialMove2: (townVertex: number, roadEdge: IEdge) => any
+    dispatchPlayerNames: (playerNames: any) => any
+    gameState: ICatanState
 }
 
 interface IAppState {
-  players: {
-    red: ICatanBot,
-    orange: ICatanBot,
-    green: ICatanBot,
-    blue: ICatanBot
-  }
+    players: {
+        red: ICatanBot
+        orange: ICatanBot
+        green: ICatanBot
+        blue: ICatanBot
+    }
 }
 
 const mapStateToProps = (state: ICatanState, ownProps: IAppProps) => ({
-  ...ownProps,
-  gameState : state
-});
+    ...ownProps,
+    gameState: state,
+})
 
-
-const mapDispatchToProps = (dispatch: any, ownProps:any) => ({
-  dispatchBotMove: (botAction: IBotMakeTurnAction) => dispatch(botAction),
-  dispatchBuildRoad: (targetEdge: IEdge) => dispatch(buildRoad(targetEdge)),
-  dispatchBuildTown: (targetVtx: number) => dispatch(buildTown(targetVtx)),
-  dispatchDistributeResources: (dieRoll: number) => dispatch(distributeResources(dieRoll)),
-  dispatchEndPlayerTurn: () => dispatch(endPlayerTurn()),
-  dispatchInitialMove1: (townVertex: number, roadEdge: IEdge) => dispatch(initialMove1(townVertex, roadEdge)),
-  dispatchInitialMove2: (townVertex: number, roadEdge: IEdge) => dispatch(initialMove2(townVertex, roadEdge)),
-  dispatchPlayerNames: (playerNames: any) => dispatch(setPlayerNames(playerNames))
-});
+const mapDispatchToProps = (dispatch: any, ownProps: any) => ({
+    dispatchBotMove: (botAction: IBotMakeTurnAction) => dispatch(botAction),
+    dispatchBuildRoad: (targetEdge: IEdge) => dispatch(buildRoad(targetEdge)),
+    dispatchBuildTown: (targetVtx: number) => dispatch(buildTown(targetVtx)),
+    dispatchDistributeResources: (dieRoll: number) =>
+        dispatch(distributeResources(dieRoll)),
+    dispatchEndPlayerTurn: () => dispatch(endPlayerTurn()),
+    dispatchInitialMove1: (townVertex: number, roadEdge: IEdge) =>
+        dispatch(initialMove1(townVertex, roadEdge)),
+    dispatchInitialMove2: (townVertex: number, roadEdge: IEdge) =>
+        dispatch(initialMove2(townVertex, roadEdge)),
+    dispatchPlayerNames: (playerNames: any) =>
+        dispatch(setPlayerNames(playerNames)),
+})
 
 class App extends React.Component<IAppProps, IAppState> {
-  private timerID: NodeJS.Timer;
+    private timerID: NodeJS.Timer
 
-  constructor(props: IAppProps) {
-    super(props);
-    this.state = {
-      players: {
-        blue: createSmarterBot('blue'),
-        green: createBasicBot('green'),
-        orange: createBasicBot('orange'),
-        red: createBasicBot('red')
-      }
+    constructor(props: IAppProps) {
+        super(props)
+        this.state = {
+            players: {
+                blue: createSmarterBot('blue'),
+                green: createBasicBot('green'),
+                orange: createBasicBot('orange'),
+                red: createBasicBot('red'),
+            },
+        }
+        this.props.dispatchPlayerNames({
+            blue: this.state.players.blue.getBotName(),
+            green: this.state.players.green.getBotName(),
+            orange: this.state.players.orange.getBotName(),
+            red: this.state.players.red.getBotName(),
+        })
     }
-    this.props.dispatchPlayerNames({
-        blue: this.state.players.blue.getBotName(),
-        green: this.state.players.green.getBotName(),
-        orange: this.state.players.orange.getBotName(),
-        red: this.state.players.red.getBotName()
-    });
-  }
 
-  public componentDidMount() {
-    // TODO: change to timeout after dispatches
-    this.timerID = setInterval(()=> this.tick(), 50);
-  }
-
-  public componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  public tick() {
-    /* tslint:disable */
-    console.log('tick');
-    /* tslint:enable */ 
-    
-    const turn = this.props.gameState.turn;
-    const turnSubAction = this.props.gameState.turnSubAction;
-    const numPlayers = this.props.gameState.playerColors.length;
-    const currentColor = getCurrentPlayerColor(this.props.gameState);
-    const currentPlayer = this.state.players[currentColor];
-
-    if (playerHasWon(this.props.gameState)) {
-      clearInterval(this.timerID);
-      // GAME OVER
+    public componentDidMount() {
+        // TODO: change to timeout after dispatches
+        this.timerID = setInterval(() => this.tick(), 1)
     }
-    if (turn >= 0 && turn < numPlayers) {
-      // INITIAL MOVES 1
-      const {townVertex, roadEdge} = currentPlayer.makeInitialMove1(this.props.gameState);
-      this.props.dispatchInitialMove1(townVertex, roadEdge);
-      this.props.dispatchEndPlayerTurn();
-    } else if (turn >= numPlayers && turn < numPlayers*2) {
-      // INITIAL MOVES 2
-      const {townVertex, roadEdge} = currentPlayer.makeInitialMove2(this.props.gameState);
-      this.props.dispatchInitialMove2(townVertex, roadEdge);
-      this.props.dispatchEndPlayerTurn();
-    } else if (turnSubAction === 0) {
-      // NORMAL TURN - DISTRIBUTE RESOURCES
-      // roll dice
-      const dieRoll = rollADie() + rollADie();
-      // tslint:disable  
-      console.log(currentColor)
-      // tslint:enable  
-      // distribute resources or move thief
-      if (dieRoll !== 7){
-        
-        this.props.dispatchDistributeResources(dieRoll);
-      } else {
-        // players with too many cards choose what to keep/discard
-        /*
+
+    public componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+    public tick() {
+        /* tslint:disable */
+        console.log('tick')
+        /* tslint:enable */
+
+        const turn = this.props.gameState.turn
+        const turnSubAction = this.props.gameState.turnSubAction
+        const numPlayers = this.props.gameState.playerColors.length
+        const currentColor = getCurrentPlayerColor(this.props.gameState)
+        const currentPlayer = this.state.players[currentColor]
+
+        if (playerHasWon(this.props.gameState)) {
+            clearInterval(this.timerID)
+            // GAME OVER
+        }
+        if (turn >= 0 && turn < numPlayers) {
+            // INITIAL MOVES 1
+            const { townVertex, roadEdge } = currentPlayer.makeInitialMove1(
+                this.props.gameState
+            )
+            this.props.dispatchInitialMove1(townVertex, roadEdge)
+            this.props.dispatchEndPlayerTurn()
+        } else if (turn >= numPlayers && turn < numPlayers * 2) {
+            // INITIAL MOVES 2
+            const { townVertex, roadEdge } = currentPlayer.makeInitialMove2(
+                this.props.gameState
+            )
+            this.props.dispatchInitialMove2(townVertex, roadEdge)
+            this.props.dispatchEndPlayerTurn()
+        } else if (turnSubAction === 0) {
+            // NORMAL TURN - DISTRIBUTE RESOURCES
+            // roll dice
+            const dieRoll = rollADie() + rollADie()
+            // tslint:disable
+            console.log(currentColor)
+            // tslint:enable
+            // distribute resources or move thief
+            if (dieRoll !== 7) {
+                this.props.dispatchDistributeResources(dieRoll)
+            } else {
+                // players with too many cards choose what to keep/discard
+                /*
 
         currentPlayer.moveThief();
         dispatch(moveThief({})); 
         */
-      }
-    } else {
-      // NORMAL TURN - PLAYER ACTIONS
-      // get actions from user until user ends turn
-      const nextAction = currentPlayer.makeTurn(this.props.gameState);
-      // tslint:disable  
-      console.log(nextAction)
-      // tslint:enable  
-      // double check action types
-      switch(nextAction.type) {
-        case BUILD_ROAD:
-        case BUILD_TOWN:
-        case UPGRADE_TOWN:
-        case BANK_TRADE:
-          this.props.dispatchBotMove(nextAction);
-        case BUILD_DEVELOPMENT_CARD:
-        case END_PLAYER_TURN:
-        default:
-          this.props.dispatchEndPlayerTurn();
-      }
-    } 
-  }
+            }
+        } else {
+            // NORMAL TURN - PLAYER ACTIONS
+            // get actions from user until user ends turn
+            const nextAction = currentPlayer.makeTurn(this.props.gameState)
+            // tslint:disable
+            console.log(nextAction)
+            // tslint:enable
+            // double check action types
+            switch (nextAction.type) {
+                case BUILD_ROAD:
+                case BUILD_TOWN:
+                case UPGRADE_TOWN:
+                case BANK_TRADE:
+                    this.props.dispatchBotMove(nextAction)
+                case BUILD_DEVELOPMENT_CARD:
+                case END_PLAYER_TURN:
+                default:
+                    this.props.dispatchEndPlayerTurn()
+            }
+        }
+    }
 
-  public render() {
+    public render() {
+        return (
+            <div className="App">
+                <h1 className="App-title">Settlers of Catan</h1>
+                <p className="App-intro" />
+                <div className="game-container">
+                    <div className="board-scores-container">
+                        <Board />
+                        <Scores />
+                    </div>
+                    <div className="narrative-container">
+                        <Narrative />
+                    </div>
+                </div>
+                <span>SVGs from http://game-icons.net/games/catan.html</span>
 
-    return (
-      <div className="App">
-        <h1 className="App-title">Settlers of Catan</h1>
-        <p className="App-intro"/>
-        <div className="game-container">
-          <div className="board-scores-container">
-            <Board/>
-            <Scores/>
-          </div>
-          <div className="narrative-container">
-            <Narrative/>
-          </div>
-        </div>
-        <span>SVGs from http://game-icons.net/games/catan.html</span>
-        
-        
-        <svg width="120" height="140">
-          <symbol id="houseIcon" viewBox="0 0 512 512">
-            <g>
-            <path d="M256 19.27L25.637 249.638 19.27 256 32 268.73l6.363-6.367L256 44.727l217.637 217.636L480 268.73 492.73 256l-6.367-6.363zM96 48v107.273l64-64.002V48zm160 20.727l-192 192V486h64V320h96v166h224V260.727zM288 320h96v80h-96z" fill="#fff" fillOpacity="1"/>
-            </g>
-          </symbol>
-          <symbol id="villageIcon" viewBox="0 0 512 512">
-            <g>
-              <path d="M109.902 35.87l-71.14 59.284h142.28l-71.14-59.285zm288 32l-71.14 59.284h142.28l-71.14-59.285zM228.73 84.403l-108.9 90.75h217.8l-108.9-90.75zm-173.828 28.75v62h36.81l73.19-60.992v-1.008h-110zm23 14h16v18h-16v-18zm265 18v10.963l23 19.166v-16.13h16v18h-13.756l.104.087 19.098 15.914h-44.446v14h78v-39h18v39h14v-62h-110zm-194.345 48v20.08l24.095-20.08h-24.095zm28.158 0l105.1 87.582 27.087-22.574v-65.008H176.715zm74.683 14h35.735v34h-35.735v-34zm-76.714 7.74L30.37 335.153H319l-144.314-120.26zm198.046 13.51l-76.857 64.047 32.043 26.704H481.63l-108.9-90.75zm-23.214 108.75l.103.086 19.095 15.914h-72.248v77.467h60.435v-63.466h50v63.467h46v-93.466H349.516zm-278.614 16V476.13h126v-76.976h50v76.977h31.565V353.155H70.902zm30 30h50v50h-50v-50z"  fill="#fff" fillOpacity="1"/>
-            </g>
-          </symbol> 
-        </svg>       
-      </div>
-      
-    );
-  }
+                <svg width="120" height="140">
+                    <symbol id="houseIcon" viewBox="0 0 512 512">
+                        <g>
+                            <path
+                                d="M256 19.27L25.637 249.638 19.27 256 32 268.73l6.363-6.367L256 44.727l217.637 217.636L480 268.73 492.73 256l-6.367-6.363zM96 48v107.273l64-64.002V48zm160 20.727l-192 192V486h64V320h96v166h224V260.727zM288 320h96v80h-96z"
+                                fill="#fff"
+                                fillOpacity="1"
+                            />
+                        </g>
+                    </symbol>
+                    <symbol id="villageIcon" viewBox="0 0 512 512">
+                        <g>
+                            <path
+                                d="M109.902 35.87l-71.14 59.284h142.28l-71.14-59.285zm288 32l-71.14 59.284h142.28l-71.14-59.285zM228.73 84.403l-108.9 90.75h217.8l-108.9-90.75zm-173.828 28.75v62h36.81l73.19-60.992v-1.008h-110zm23 14h16v18h-16v-18zm265 18v10.963l23 19.166v-16.13h16v18h-13.756l.104.087 19.098 15.914h-44.446v14h78v-39h18v39h14v-62h-110zm-194.345 48v20.08l24.095-20.08h-24.095zm28.158 0l105.1 87.582 27.087-22.574v-65.008H176.715zm74.683 14h35.735v34h-35.735v-34zm-76.714 7.74L30.37 335.153H319l-144.314-120.26zm198.046 13.51l-76.857 64.047 32.043 26.704H481.63l-108.9-90.75zm-23.214 108.75l.103.086 19.095 15.914h-72.248v77.467h60.435v-63.466h50v63.467h46v-93.466H349.516zm-278.614 16V476.13h126v-76.976h50v76.977h31.565V353.155H70.902zm30 30h50v50h-50v-50z"
+                                fill="#fff"
+                                fillOpacity="1"
+                            />
+                        </g>
+                    </symbol>
+                </svg>
+            </div>
+        )
+    }
 }
 
-export default connect<IAppProps>(mapStateToProps, mapDispatchToProps)(App);
+export default connect<IAppProps>(mapStateToProps, mapDispatchToProps)(App)
--- src/bots/smarterbot.bot.ts
+++ src/bots/smarterbot.bot.ts
@@ -2,63 +2,79 @@ import { Color, ICatanState } from '../types';
 import { ICatanBot } from '../types/bot';
 import { Bot } from './bot';
 
-/* BASIC BOT 
-    - WILL make random (but valid moves)
-    - if there are enough resources and valid spaces, will attempt to:
-        1. upgrade city
-        2. build town
-        3. build road
-    - if there are not enough resources to build with, will attempt to:
-        1. make a bank trade for the most lacking resource
-
-    - WILL NOT use dev cards or make player trades
-*/
-
 export const createSmarterBot = (color: Color): ICatanBot => {
-    return new SmarterBot(color);
-};
+    return new SmarterBot(color)
+}
 
 class SmarterBot extends Bot {
     constructor(color: Color) {
-        super(color);
-        
+        super(color)
     }
 
-    public makeInitialMove1 (state: ICatanState) {return this.basicInitialMove(state);}
-    public makeInitialMove2 (state: ICatanState) {return this.basicInitialMove(state);}
-
-
-    public getBotName () {
-        return 'Smarter Bot ' + this.savedRandomNum;
+    public makeInitialMove1(state: ICatanState) {
+        return this.basicInitialMove(state)
+    }
+    public makeInitialMove2(state: ICatanState) {
+        return this.basicInitialMove(state)
     }
 
-    protected basicInitialMove (state: ICatanState)  {
-        const validTowns = this.getValidInitTownLocations(state, this.getOwnColor());
-        const sortedTowns = validTowns.sort((a,b) => {
-            if(state.vertexAdjacentHexes[a].find(h => h.dieRoll === 7))
-                {return 1;}
+    public getBotName() {
+        return 'Smarter Bot ' + this.savedRandomNum
+    }
 
-            if(state.vertexAdjacentHexes[b].find(h => h.dieRoll === 7))
-               { return -1;}
+    protected sortTowns = (towns: number[], state: ICatanState): number[] => {
+        const sortedTowns = towns.sort((a, b) => {
+            if (state.vertexAdjacentHexes[a].find((h) => h.dieRoll === 7)) {
+                return -1
+            }
 
-            let rollProbabilityA = 0; 
-            state.vertexAdjacentHexes[a].forEach(h => rollProbabilityA += (7 - Math.abs(7 - h.dieRoll)));
-            
-            let rollProbabilityB = 0; 
-            state.vertexAdjacentHexes[b].forEach(h => rollProbabilityB += (7 - Math.abs(7 - h.dieRoll)));
+            if (state.vertexAdjacentHexes[b].find((h) => h.dieRoll === 7)) {
+                return 1
+            }
 
-            return rollProbabilityB - rollProbabilityA;
-        });
-        const townVertex = sortedTowns[0];
+            let rollProbabilityA = 0
+            state.vertexAdjacentHexes[a].forEach(
+                (h) => (rollProbabilityA += 7 - Math.abs(7 - h.dieRoll))
+            )
+
+            let rollProbabilityB = 0
+            state.vertexAdjacentHexes[b].forEach(
+                (h) => (rollProbabilityB += 7 - Math.abs(7 - h.dieRoll))
+            )
+
+            return rollProbabilityA - rollProbabilityB
+        })
+
+        return sortedTowns
+    }
+
+    protected basicInitialMove(state: ICatanState) {
+        const validTowns = this.getValidInitTownLocations(
+            state,
+            this.getOwnColor()
+        )
+        const sortedTowns = this.sortTowns(validTowns, state)
+        const townVertex = sortedTowns.pop() as number
         const adjacentRoads = state.allEdges
-        .filter(e => e[0] === townVertex || e[1] === townVertex)
-        .filter(e => typeof state.roads.find(r => r.edge === e) === 'undefined');
-        const randomRoadIdx = Math.floor(Math.random() * adjacentRoads.length);
-        const roadEdge = adjacentRoads[randomRoadIdx];
+            .filter((e) => e[0] === townVertex || e[1] === townVertex)
+            .filter(
+                (e) =>
+                    typeof state.roads.find((r) => r.edge === e) === 'undefined'
+            )
+        const randomRoadIdx = Math.floor(Math.random() * adjacentRoads.length)
+        const roadEdge = adjacentRoads[randomRoadIdx]
 
         return {
             roadEdge,
-            townVertex
-        };
+            townVertex,
+        }
+    }
+
+    protected buildTown(validTowns: number[], state: ICatanState) {
+        const bestTown = this.sortTowns(validTowns, state).pop() as number
+        return {
+            targetVtx: bestTown,
+            type: 'BUILD_TOWN',
+        }
     }
 }
